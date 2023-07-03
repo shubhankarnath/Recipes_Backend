8b567bd3b1ec50e77fd347bd44baee2f0a7f09a5
shubhankarnath <87268913+shubhankarnath@users.noreply.github.com>
2023-07-03 16:13:43 +0530

    Update config.js
    d

--- server/Db/config.js
+++ server/Db/config.js
@@ -5,8 +5,7 @@ mongoose.set("strictQuery", true);
 const databaseUrl = process.env.DATABASE_URL;
 
 mongoose.connect(databaseUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  
 }).then(() => {
   console.log('Connected to database');
 }).catch((error) => {
